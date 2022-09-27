c0335d9591be062ced7e45b415fb99b1c2d5b21b
Calvin Wilkinson <kinsondigital@gmail.com>
2022-09-27 15:07:54 +0100

    Create project

---
+++ DotNetToolTesting/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Reflection;
+
+var executePath = Assembly.GetExecutingAssembly().Location;
+var entryPath = Assembly.GetEntryAssembly().Location;
+var callingPath = Assembly.GetCallingAssembly().Location;
+var currentWorkingDir = Directory.GetCurrentDirectory();
+
+Console.WriteLine($"Working Dir: {currentWorkingDir}");
+Console.WriteLine($"Execute Path: {executePath}");
+Console.WriteLine($"Entry Path: {entryPath}");
+Console.WriteLine($"Calling Path: {callingPath}");
